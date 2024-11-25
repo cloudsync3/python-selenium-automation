d19e4edeb8dbe10044659fcbc623ab5b9abecd6c
Dasha N <dasha.nesterova@yahoo.com>
2024-11-25 19:27:48 +0800

    Homework2

---
+++ features/tests/target_search.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.target.com')
+
+# Search
+driver.find_element(By.ID, 'search').send_keys('basket')
+driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']").click()
+sleep(6)
+
+#Verification
+expected_result = 'basket'
+actual_result = driver.find_element(By.XPATH, "//span[@class='h-text-bs h-display-flex h-flex-align-center h-text-grayDark h-margin-l-x2']").text
+assert expected_result in actual_result, f'Expected test {expected_result} not in actual {actual_result}'
+
+print('Test case passed')
+
+driver.quit()
+
+
+
