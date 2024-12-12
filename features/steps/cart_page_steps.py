9eaf119ad0bbbae5675627f433d136c5fbebf494
Dasha N <dasha.nesterova@yahoo.com>
2024-12-12 16:50:50 +0800

    Updated Hmk 4

--- features/steps/cart_page_steps.py
+++ features/steps/cart_page_steps.py
@@ -3,11 +3,6 @@ from behave import given, when, then
 from time import sleep
 
 
-# @given('Open target main page')
-# def open_main(context):
-#     context.driver.get('https://www.target.com')
-
-
 @then('Verify cart is empty message')
 def step_verify_cart_message(context):
     expected = 'Your cart is empty'
--- features/steps/signin_steps.py
+++ features/steps/signin_steps.py
@@ -2,10 +2,6 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
 
-# @given('Open target main page')
-# def open_main(context):
-#     context.driver.get('https://www.target.com')
-
 
 @when('Click on the Sign in button')
 def click_signin_button(context):
